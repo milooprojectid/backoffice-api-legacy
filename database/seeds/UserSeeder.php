15c13e9ac1a76b6b377cf8b246f1c6b642bc500d
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2018-11-08 18:48:14 +0700

    add authentication scafolding

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create(['name' => 'admin', 'email' => 'sakojudev@gmail.com', 'password' => Hash::make('admin')]);
+    }
+}
