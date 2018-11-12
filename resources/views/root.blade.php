ba6fa164706b4c5710db0a37128915e8ee1924cb
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2018-11-12 19:50:51 +0700

    add scripts

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -1,10 +1,19 @@
 <!doctype html>
 <html lang="en">
-@include('layout.header_script')
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="refresh" content="1800"/>
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <link rel="icon" type="image/gif" href="{{asset('/img/logo-min.png')}}" />
+    <title>@yield('page_title', 'Milo Backoffice')</title>
+    <link rel="stylesheet" href="{{asset('/css/min.css')}}">
+</head>
 <body class="{{ Request::is('login') ? 'hold-transition login-page' : 'hold-transition skin-yellow-light sidebar-mini fixed' }}">
     <div id="milo-app">
         <App/>
     </div>
 </body>
-@include('layout.footer_script')
+<script src="{{asset('/js/app.js')}}"></script>
+<script src="{{asset('/js/min.js')}}"></script>
 </html>
