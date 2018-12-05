bb397544409714ca6c15c8dad97b1f50c0c46047
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2018-12-05 18:17:22 +0700

    update assets

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="refresh" content="1800"/>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <link rel="icon" href="{{asset('/img/logo-min.png')}}" />
+    <link rel="icon" href="/img/logo-min.png" />
     <title>@yield('page_title', 'Milo Backoffice')</title>
     <link rel="stylesheet" href="/css/min.css">
 </head>
